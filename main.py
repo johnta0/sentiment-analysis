b3833a2f61b950df3971691fe93d9a655fa74f8c
johnta0 <22534726+johnta0@users.noreply.github.com>
2019-01-07 00:52:15 +0900

    impl analizing and scoring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import mecab
+import MeCab
 
 def dicfile_to_dict():
     # fuck shift-jis encoding :middle_finger:
@@ -14,15 +14,35 @@ def dicfile_to_dict():
     return dict(zip(words, score))
 
 def calc_score(word, dic):
+    if not word in dic:
+        return None
     return dic[word]
-
-def 
 
 def main():
     # set up dictionary
     dic = dicfile_to_dict()
     # set up data to analyze
-    text = 'ありがとうございました。好きです。'
+    text = '何らかの文章'
     # morphological analysic by mecab
+    tagger = MeCab.Tagger("-Ochasen")
+    parsed_arr = tagger.parse(text).splitlines()
+    parsed_arr.pop()    
+
+    total = 0
+    for word in parsed_arr:
+        # split by tab
+        tab = word.split('\t')
+        # use word, which is the 0th element of tab(array)
+        score = calc_score(tab[0], dic)
+        if score is None:
+            total += 0
+            print('Word not found in the library')
+        else:
+            print('Score: ', end="")
+            print(score)
+            total += score
+    print('Total Score: ')
+    print(total)
 
 if __name__ == '__main__':
+    main()
